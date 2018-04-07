4fdfb6a45283dd4c4ea663cd3af8172022b3febc
laochanlam <laochanlam@gmail.com>
2018-04-07 19:17:23 +0800

    perform Gaussian blur

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate image;
+
 fn main() {
-    println!("Hello, world!");
+    let img = image::open("data/input.bmp").unwrap();
+    let sigma = 5f32;
+    //TODO: figure out the value of sigma
+    let img_out = img.blur(sigma);
+    let ref mut fout = std::fs::File::create("data/output.bmp").unwrap();
+    img_out.save(fout, image::BMP).unwrap();
+
 }
