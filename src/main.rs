fe3d26512e292088ce6bb21fee910699c4b4a685
etc276 <etc276@gmail.com>
2018-04-15 19:09:04 +0800

    Add new function (just try)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 extern crate image;
+extern crate num;
+extern crate num_traits;
+
+mod cayon;   // import test.rs
 
 fn main() {
     let img = image::open("data/input.bmp").unwrap();
+
+    // call library
+    // TODO: figure out the value of sigma
     let sigma = 5f32;
-    //TODO: figure out the value of sigma
-    let img_out = img.blur(sigma);
-    let ref mut fout = std::fs::File::create("data/output.bmp").unwrap();
-    img_out.save(fout, image::BMP).unwrap();
+    let blur_img = img.blur(sigma);
+    let ref mut fout = std::fs::File::create("data/blurred.bmp").unwrap();
+    blur_img.save(fout, image::BMP).unwrap();
+
+    // call my function
+    let red_img = cayon::redden(img);
+    red_img.save("data/redden.bmp");
 
 }
