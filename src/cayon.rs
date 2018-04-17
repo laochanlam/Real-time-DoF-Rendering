85ce50f280737b636fb2667167f4db0c85ff26b5
etc276 <etc276@gmail.com>
2018-04-17 11:35:21 +0800

    add function for count CoC, (input, output) = (img, vec)

--- src/cayon.rs
+++ src/cayon.rs
@@ -3,6 +3,31 @@ use image:: {GenericImage, ImageBuffer, Pixel};
 use num::NumCast;
 use num_traits::clamp;
 
+pub fn count_CoC <I: GenericImage> (img: I) -> Vec<u8> 
+    where I::Pixel: 'static {
+    
+    let (width, height) = img.dimensions();
+    let mut a = vec![0; NumCast::from(width * height).unwrap()];
+
+    for x in 0..width {
+        for y in 0..height {
+            let px = img.get_pixel(x, y);
+            let (k1, _, _, _) = px.channels4();
+
+            let x: f64 = NumCast::from(k1).unwrap();
+            let mut y: u8 = (x + 1.0).sqrt() as u8;
+            if y % 2 == 0 {
+                y += 1;
+            }
+            a.push(NumCast::from(y).unwrap());
+            // assert_eq!(a[a.len()-1], 0);
+        }
+    }
+
+    // return
+    a
+}
+
 pub fn redden <I: GenericImage> (img: I)                            // first line is template and parameter 
     -> ImageBuffer<I::Pixel, Vec<<I::Pixel as Pixel>::Subpixel>>    // second line is return value
     where I::Pixel: 'static {                                       // third line (and after) is where clause
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,21 @@ mod cayon;   // import test.rs
 
 fn main() {
     let img = image::open("data/input.bmp").unwrap();
+    let b = cayon::count_CoC(img);
 
     // call library
     // TODO: figure out the value of sigma
+    /*
     let sigma = 5f32;
     let blur_img = img.blur(sigma);
     let ref mut fout = std::fs::File::create("data/blurred.bmp").unwrap();
     blur_img.save(fout, image::BMP).unwrap();
+    */
 
     // call my function
+    /*
     let red_img = cayon::redden(img);
     red_img.save("data/redden.bmp");
+    */
 
 }
