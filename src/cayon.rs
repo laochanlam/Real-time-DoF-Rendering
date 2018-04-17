b80697df57fb8a5afd74bb9b91c53fdd17112181
Cayonliow <cayon.1318.96@hotmail.com>
2018-04-18 03:00:06 +0800

    mipmap level 1 is done

--- src/cayon.rs
+++ src/cayon.rs
@@ -3,77 +3,61 @@ use image:: {GenericImage, ImageBuffer, Pixel};
 use num::NumCast;
 use num_traits::clamp;
 
+pub fn get_rgb <P: Pixel> (px: P)
+	-> (f64, f64, f64) 
+	where P::Subpixel: 'static {
 
-struct rgb{
-	r: i32,
-	g: i32,
-	b: i32,
-	wtf: i32
+	let (k1, k2, k3, k4) = px.channels4();
+	let vec: (f64, f64, f64, f64) = (
+		NumCast::from(k1).unwrap(),
+		NumCast::from(k2).unwrap(),
+		NumCast::from(k3).unwrap(),
+		NumCast::from(k4).unwrap()
+	);
+
+	let mut r = vec.0;
+	let mut g = vec.1;
+	let mut b = vec.2;
+
+	// return 
+	(r,g,b)
 }
 
-
-struct arrHh{
-	arrH: [rgb; 1000000]
-}
 
 pub fn downsize <I: GenericImage> (img: I, lvl: u32) 
     -> ImageBuffer<I::Pixel, Vec<<I::Pixel as Pixel>::Subpixel>>   
     where I::Pixel: 'static {                                      
 	let two: u32 =2;
     let (width, height) = img.dimensions();
-   // let mut down_img = ImageBuffer::new(width/(two.pow(lvl)), height/(two.pow(lvl)));
 
-    let mut down_img = ImageBuffer::new(width, height);
-        
-	let arrW: [arrHh; 1000000]; 
+    let mut down_img = ImageBuffer::new(width/two, height/two);
 
-	
-	for x in 0..width {
-        for y in 0..height {
-            let px = img.get_pixel(x, y);
-            let (k1, k2, k3, k4) = px.channels4();
-            let vec: (f64, f64, f64, f64) = (
-                NumCast::from(k1).unwrap(),
-                NumCast::from(k2).unwrap(),
-                NumCast::from(k3).unwrap(),
-                NumCast::from(k4).unwrap()
-            );
+	for n in 0..lvl {
+		for x in 0..width/2 {
+			for y in 0..height/2 {
+				let px00 = img.get_pixel(2*x, 2*y);
+				let px01 = img.get_pixel(2*x, 2*y+1);
+				let px10 = img.get_pixel(2*x+1, 2*y);
+				let px11 = img.get_pixel(2*x+1, 2*y+1);
 
-            arrW[x/two].arrHh.arrH[y/two].rgb.r += vec.0;
-            arrW[x/two].arrHh.arrH[y/two].rgb.g += vec.1;
-            arrW[x/two].arrHh.arrH[y/two].rgb.b += vec.2;
-            arrW[x/two].arrHh.arrH[y/two].rgb.wtf += vec.3;
+				let (r1, g1, b1) = get_rgb (px00);
+				let (r2, g2, b2) = get_rgb (px01);
+				let (r3, g3, b3) = get_rgb (px10);
+				let (r4, g4, b4) = get_rgb (px11);
 
-			
 
-/*            let new_pixel = Pixel::from_channels(
-                NumCast::from(clamp(r, 0.0, 255.0)).unwrap(),
-                NumCast::from(clamp(g, 0.0, 255.0)).unwrap(),
-                NumCast::from(clamp(b, 0.0, 255.0)).unwrap(),
-                NumCast::from(clamp(wtf, 0.0, 255.0)).unwrap()
-			);
-*/
+            	let new_pixel = Pixel::from_channels(
+                	NumCast::from(clamp((r1+r2+r3+r4)/4.0, 0.0, 255.0)).unwrap(),
+					NumCast::from(clamp((g1+g2+g3+g4)/4.0, 0.0, 255.0)).unwrap(),
+					NumCast::from(clamp((b1+b2+b3+b4)/4.0, 0.0, 255.0)).unwrap(),
+					NumCast::from(clamp(0.0, 0.0, 255.0)).unwrap(),
+				);
 
-		//	img_out.put_pixel(x, y, new_pixel);
+				down_img.put_pixel(x, y, new_pixel);
+			}
 		}
 	}
-
-	for x in 0..width/two {
-        for y in 0..height/two {
-			arrW[x].arrHh.arrH[y].rgb.r /= 4;
-			arrW[x].arrHh.arrH[y].rgb.g /= 4;
-			arrW[x].arrHh.arrH[y].rgb.b /= 4;
-			arrW[x].arrHh.arrH[y].rgb.wtf /= 4;
-
-            let new_pixel = Pixel::from_channels(
-                NumCast::from(clamp(arrW[x].arrHh.arrH[y].rgb.r, 0.0, 255.0)).unwrap(),
-                NumCast::from(clamp(arrW[x].arrHh.arrH[y].rgb.g, 0.0, 255.0)).unwrap(),
-                NumCast::from(clamp(arrW[x].arrHh.arrH[y].rgb.b, 0.0, 255.0)).unwrap(),
-                NumCast::from(clamp(arrW[x].arrHh.arrH[y].rgb.wtf, 0.0, 255.0)).unwrap()
-			);
-			down_img.put_pixel(x, y, new_pixel);
-		}
-	}
+  
 	//return
 	down_img
-	}
+}
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,26 @@ extern crate num_traits;
 mod cayon;   // import test.rs
 
 fn main() {
-    let img = image::open("data/input.bmp").unwrap();
 
+    let mipmap_level: [f32; 5] = [1.46, 3.05, 6.2, 13.05, 24.83];
+    let mut pathblur: [String; 5] = ["data/blurred1.bmp".to_string(), "data/blurred2.bmp".to_string(), "data/blurred3.bmp".to_string(), "data/blurred4.bmp".to_string(), "data/blurred5.bmp".to_string()];
+    let mut pathdown: [String; 5] = ["data/downsize1.bmp".to_string(), "data/downsize2.bmp".to_string(), "data/downsize3.bmp".to_string(), "data/downsize4.bmp".to_string(), "data/downsize5.bmp".to_string()];
+
+
+    let mut img = image::open("data/input.bmp").unwrap();
+
+    for n in 1..5{
+        let sigma = mipmap_level[n];
+        let blur_img = img.blur(sigma);
+        //println!("{}",pathblur[n]);
+        let ref mut fout = std::fs::File::create(pathblur[n-1].to_string()).unwrap();
+        blur_img.save(fout, image::BMP).unwrap();
+
+	    //call cayon's function
+	    let downsize_img = cayon::downsize(blur_img, (n) as u32);
+        downsize_img.save(pathdown[n-1].to_string());
+    }
     // call library
     // TODO: figure out the value of sigma
-    let sigma = 5f32;
-    let blur_img = img.blur(sigma);
-    let ref mut fout = std::fs::File::create("data/blurred.bmp").unwrap();
-    blur_img.save(fout, image::BMP).unwrap();
-
-    // call my function
-    let red_img = cayon::redden(img);
-    red_img.save("data/redden.bmp");
 
 }
