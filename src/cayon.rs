5b8962fe8520bab50aaf4101d1da8fc8e5c3c88f
etc276 <etc276@gmail.com>
2018-04-23 21:41:42 +0800

    Feature: whatever

--- src/cayon.rs
+++ src/cayon.rs
@@ -3,23 +3,24 @@ use image:: {GenericImage, ImageBuffer, Pixel};
 use num::NumCast;
 use num_traits::clamp;
 
-pub fn count_CoC <I: GenericImage> (img: I) -> Vec<u8> 
+pub fn count_coc <I: GenericImage> (img: &I) -> Vec<u8> 
     where I::Pixel: 'static {
     
     let (width, height) = img.dimensions();
-    let mut a = vec![0; NumCast::from(width * height).unwrap()];
+    let mut a: Vec<u8> = vec![0; NumCast::from(width * height).unwrap()];
 
     for x in 0..width {
         for y in 0..height {
             let px = img.get_pixel(x, y);
             let (k1, _, _, _) = px.channels4();
 
-            let x: f64 = NumCast::from(k1).unwrap();
-            let mut y: u8 = (x + 1.0).sqrt() as u8;
-            if y % 2 == 0 {
-                y += 1;
+            let k1_unwrap: f64 = NumCast::from(k1).unwrap();
+            let mut radius: u32 = (k1_unwrap + 1.0).sqrt() as u32;
+            if radius % 2 == 0 {
+                radius += 1;
             }
-            a.push(NumCast::from(y).unwrap());
+            // a.push(NumCast::from(y).unwrap());
+			a[((x as u32)*height + y) as usize] = radius as u8;
             // assert_eq!(a[a.len()-1], 0);
         }
     }
@@ -28,7 +29,7 @@ pub fn count_CoC <I: GenericImage> (img: I) -> Vec<u8>
     a
 }
 
-pub fn redden <I: GenericImage> (img: I)                            // first line is template and parameter 
+pub fn redden <I: GenericImage> (img: &I)                            // first line is template and parameter 
     -> ImageBuffer<I::Pixel, Vec<<I::Pixel as Pixel>::Subpixel>>    // second line is return value
     where I::Pixel: 'static {                                       // third line (and after) is where clause
 
@@ -64,4 +65,71 @@ pub fn redden <I: GenericImage> (img: I)                            // first lin
     
     // return
     img_out
+}
+
+pub fn whatever <I: GenericImage> (img: &I, radius: &mut Vec<u8>)
+    -> ImageBuffer<I::Pixel, Vec<<I::Pixel as Pixel>::Subpixel>>
+    where I::Pixel: 'static {
+
+	let (width, height) = img.dimensions();
+	let (width, height) = (width as i32, height as i32);
+	let mut img_out = ImageBuffer::new(width as u32, height as u32);
+	const _size: usize = 1600*1200 + 1;
+	let mut pixel_r = vec![0.0; _size];
+	let mut pixel_g = vec![0.0; _size];
+	let mut pixel_b = vec![0.0; _size];
+
+	for x in 0..width {
+		for y in 0..height {
+			let px = img.get_pixel(x as u32, y as u32);
+			let (k1, k2, k3, k4) = px.channels4();
+			let tup: (f64, f64, f64, f64) = (
+				NumCast::from(k1).unwrap(),
+				NumCast::from(k2).unwrap(),
+				NumCast::from(k3).unwrap(),
+				NumCast::from(k4).unwrap()
+			);
+
+			// (0, 0, 0) is black
+			let _pos = x * height + y;
+			let r: i32 = radius[_pos as usize] as i32;
+			let x_left: i32 = x - (r-1)/2;
+			let x_right: i32 = x + (r-1)/2;
+			let y_left: i32 = y - (r-1)/2;
+			let y_right: i32 = y + (r-1)/2;
+
+			for i in x_left..x_right {
+				for j in y_left..y_right {
+					if i<0 || i>= width || j<0 || j>= height {
+						continue;
+					}
+
+					pixel_r[(i*height + j) as usize] += tup.0 / (r as f64);
+					pixel_g[(i*height + j) as usize] += tup.1 / (r as f64);
+					pixel_b[(i*height + j) as usize] += tup.2 / (r as f64);
+				}
+			}
+		}
+	}
+
+	for x in 0..width {
+		for y in 0..height {
+			let _pos = (x*height + y) as usize;
+			let r = pixel_r[_pos];
+			let g = pixel_g[_pos];
+			let b = pixel_b[_pos];
+
+			let new_pixel = Pixel::from_channels(
+                NumCast::from(clamp(r, 0.0, 255.0)).unwrap(),
+                NumCast::from(clamp(g, 0.0, 255.0)).unwrap(),
+                NumCast::from(clamp(b, 0.0, 255.0)).unwrap(),
+                NumCast::from(clamp(0.0, 0.0, 255.0)).unwrap()
+            );
+
+            img_out.put_pixel(x as u32, y as u32, new_pixel);
+		}
+	}
+
+	// return 
+	img_out
 }
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,19 @@ mod cayon;   // import test.rs
 
 fn main() {
     let img = image::open("data/input.bmp").unwrap();
-    let b = cayon::count_CoC(img);
+    let mut b = cayon::count_coc(&img);
+    let c = cayon::whatever(&img, &mut b);
+    c.save("data/haha.bmp");
 
-    // call library
-    // TODO: figure out the value of sigma
-    /*
-    let sigma = 5f32;
-    let blur_img = img.blur(sigma);
-    let ref mut fout = std::fs::File::create("data/blurred.bmp").unwrap();
-    blur_img.save(fout, image::BMP).unwrap();
-    */
+    // // call library
+    // // TODO: figure out the value of sigma
+    // let sigma = 5f32;
+    // let blur_img = img.blur(sigma);
+    // let ref mut fout = std::fs::File::create("data/blurred.bmp").unwrap();
+    // blur_img.save(fout, image::BMP).unwrap();
 
-    // call my function
-    /*
-    let red_img = cayon::redden(img);
-    red_img.save("data/redden.bmp");
-    */
+    // // call my function
+    // let red_img = cayon::redden(&img);
+    // red_img.save("data/redden.bmp");
 
 }
